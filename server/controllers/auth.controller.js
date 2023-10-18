f4c174ba34ce9aef1e7353305ab7722591303aac
sandeep <sandeepvasa025@gamil.com>
2023-10-18 21:07:19 +0530

    sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/usermodel");
 const bcrypt = require('bcrypt');
+const errorHandler = require("../utils/error");
+const jwt = require('jsonwebtoken');
 
 const signup = async(req,res,next)=>{
    const { username, email, password } =req.body;
@@ -13,6 +15,21 @@ const signup = async(req,res,next)=>{
    } catch (error) {
     next(error)
    }
-  
 }
-module.exports = {signup}
+const signIn = async(req,res,next)=>{
+   const {email,password} = req.body;
+   try {
+      const validUser = await User.findOne({email})
+      if(!validUser) return next(errorHandler(404,'User not found'));
+      const validPassword = await bcrypt.compare(password,validUser.password);
+      if(!validPassword) return next(errorHandler(401,'Password not correct'));
+      const token = jwt.sign({id:validUser._id},process.env.JWT_SECRT);
+      const {password:hashedPassword, ...rest} = validUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(rest);    
+   } catch (error) {
+      next(error);
+   }
+
+}
+module.exports = {signup,signIn}
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { signup } = require('../controllers/auth.controller');
+const { signup, signIn } = require('../controllers/auth.controller');
 const router = express.Router();
 router.post('/signup',signup);
+router.post('/signin',signIn);
 module.exports = router;
